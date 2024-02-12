cc64bac2af6ee9b510ecca8b7973f1297ed94e8f
dc552 <dc552@DESKTOP-80ATPO4>
2024-02-12 19:57:19 -0400

    Agregar archivos de proyecto.

---
+++ basexaltura_triangulo/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("");
+
+// triangulo
+
+
+//b= es la base
+//h= es la altura
+
+
+Console.WriteLine("Ingrese su base:");
+double b = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ingrese su altura:");
+double h = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine();
+
+double area = calculo(b, h);
+Console.WriteLine(area);
+
+static double calculo(double b, double h)
+
+{
+
+    double area = b * h / 2;
+    return area;
+
+}
+
